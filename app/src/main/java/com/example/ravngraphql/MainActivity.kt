769d43e0f55f8e0d08ee0f088eccfe6721ee6dfe
Jair francesco <jairfrancesco@gmail.com>
2019-10-19 10:21:32 -0500

    Added Basic Apollo query example
    -Search users example

--- app/src/main/java/com/example/ravngraphql/MainActivity.kt
+++ app/src/main/java/com/example/ravngraphql/MainActivity.kt
@@ -2,6 +2,13 @@ package com.example.ravngraphql
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ravn.UserQuery
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +18,40 @@ class MainActivity : AppCompatActivity() {
 
         var userFragment = UserSearchFragment()
         getSupportFragmentManager().beginTransaction().add(R.id.frameContainer, userFragment);
+        getUsers()
+    }
+
+    fun getUsers(){
+
+        myApolloClient.myApolloClient.query(
+            UserQuery.builder()
+                .query("jairfrancesco")
+                .build()
+        ).enqueue(object : ApolloCall.Callback<UserQuery.Data>() {
+
+            override fun onResponse(dataResponse: Response<UserQuery.Data>) {
+
+                val buffer = StringBuffer()
+                val users = dataResponse.data()?.search()?.edges()
+
+                users!!.forEach {
+                    buffer.append("name:" + it.node()?.toString())
+                    buffer.append("\n~~~~~~~~~~~")
+                    buffer.append("\n\n")
+                }
+
+
+                // onResponse returns on a background thread. If you want to make UI updates make sure they are done on the Main Thread.
+                this@MainActivity.runOnUiThread {
+                    val txtResponse = findViewById<TextView>(R.id.txtTest)
+                    txtResponse.text = buffer.toString()
+                }
+
+            }
+
+            override fun onFailure(e: ApolloException) {
+                Log.d("TAG",e.message.toString())
+            }
+        })
     }
 }
