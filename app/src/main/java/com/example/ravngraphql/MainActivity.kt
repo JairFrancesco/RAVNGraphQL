42d67564496028e969ad7c10f1474fcf6a9d35de
Jair francesco <jairfrancesco@gmail.com>
2019-10-19 09:12:42 -0500

    Added basic user interface

--- app/src/main/java/com/example/ravngraphql/MainActivity.kt
+++ app/src/main/java/com/example/ravngraphql/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var userFragment = UserSearchFragment()
+        getSupportFragmentManager().beginTransaction().add(R.id.frameContainer, userFragment);
     }
 }
